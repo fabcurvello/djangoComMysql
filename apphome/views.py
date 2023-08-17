afe0469c800f1c9774cda2e3f9dedfad796a6c48
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-17 13:53:48 -0300

    Definindo e configurando as views

--- apphome/views.py
+++ apphome/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index_home(request):
+    return render(request, 'index.html')
+
+
+def contato(request):
+    return render(request, 'contato.html')
+
+
+def produto(request):
+    return render(request, 'produto.html')
+
