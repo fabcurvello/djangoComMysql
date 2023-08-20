9e6a9b341aabd1e91c27bc2149f7608c626b60c4
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-20 13:02:46 -0300

    forms.oy criado na apphome, com a classe ContatoForm

---
+++ apphome/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+
+class ContatoForm(forms.Form):
+    nome = forms.CharField(label='Nome', max_length=80)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    assunto = forms.CharField(label='Assunto', max_length=150)
+    mensagem = forms.CharField(label='Mensagem', max_length=255, widget=forms.Textarea())
+
+
+
--- apphome/views.py
+++ apphome/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ContatoForm
 
 
 def index_home(request):
