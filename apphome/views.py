bf94e0855f3c161ff9e366045e1de6ab184698eb
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2023-08-20 14:22:19 -0300

    views.py contato melhorado

--- apphome/views.py
+++ apphome/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.shortcuts import render
+
 from .forms import ContatoForm
 
 
@@ -7,7 +9,21 @@ def index_home(request):
 
 
 def contato(request):
-    form = ContatoForm()
+    form = ContatoForm(request.POST or None) # ou recebe o que o usuário vai enviar ou none que é quando a página é carregada ainda vazia
+
+    if (str(request.method) == 'POST'):
+        if (form.is_valid()):
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print(f"-- Mensagem eviada: \nNome: {nome} \nE-mail: {email} \nAssunto: {assunto} \nMensagem: {mensagem} \n--")
+
+            messages.success(request, 'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar e-mail')
 
     context = {
         'form': form
